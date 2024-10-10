8c23331cce1a89dfdd034dcb1117464423641799
spyder227 <rickard.nicole@hotmail.com>
2024-10-10 10:48:17 -0400

    updated settings.js

--- source/js/settings.js
+++ source/js/settings.js
@@ -1,6 +1,6 @@
-const deployID = 'AKfycbyU27BN16nzFbO0Vuboidv68ZpXXZ1vbNmR_vY25el_ijq3cliInm-6K5LC9McTbcrv';
-const sheetID = `1khon8SsEWHCd4QA9Vgal7RFNy_Y_yHtAzvzUIFVa6Ho`;
-const oldSheetID = `1KDKs6Kh7dXd9V3Vgcw9ipLPiaDsUqiyRPNGYw7wFnsQ`;
+const deployID = 'AKfycbxEgnX_a6btHE0FUDOkliMnYkQp2U0-ga4jbIgaX-oQgiBrIzzbjH6TNjJw8-K7VR19';
+const sheetID = `1HGxrc3juJYQpNCYSfjxSSUjVUVC7ddT6h0rA3REP0qY`;
+const oldSheetID = `xxx`;
 let successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
-const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
+const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers", "notlux"];
