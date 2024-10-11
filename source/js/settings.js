235915464d79a90c81f6ee004a26999b3aa5bbd4
Dawn Eggleton <dawn.eggleton@Dawns-MacBook-Air.local>
2024-10-11 20:03:18 -0300

    final touches

--- source/js/settings.js
+++ source/js/settings.js
@@ -1,7 +1,7 @@
 const deployID = 'AKfycbyU27BN16nzFbO0Vuboidv68ZpXXZ1vbNmR_vY25el_ijq3cliInm-6K5LC9McTbcrv';
 const sheetID = `1khon8SsEWHCd4QA9Vgal7RFNy_Y_yHtAzvzUIFVa6Ho`;
 const oldSheetID = `1KDKs6Kh7dXd9V3Vgcw9ipLPiaDsUqiyRPNGYw7wFnsQ`;
-let successMessage = `<p class="fullWidth">Submission successful!</p>
+const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
 const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
-const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#84ad85', '#c9986e', '#b6aa79', '#ba7878'];
+const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
