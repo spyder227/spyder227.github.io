2ae9c6f7ee991da55d697418509fa4a4372545e6
Spyder <rickard.nicole@hotmail.com>
2024-12-30 15:55:46 -0500

    added thread tags

--- dist/js/settings.js
+++ dist/js/settings.js
@@ -3,5 +3,5 @@ const sheetID = `1HGxrc3juJYQpNCYSfjxSSUjVUVC7ddT6h0rA3REP0qY`;
 const oldSheetID = `xxx`;
 let successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
-const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers", "notlux"];
+const threadTags = ["vital", "rapidfire", "meetcute", "romantic", "family", "friends", "enemies", "coworkers", "notlux"];
 const chartColors = ['#74b381', '#408f8f', '#816e8f', '#a39450', '#a15a6f', '#c9986e', '#b6aa79', '#ba7878'];
