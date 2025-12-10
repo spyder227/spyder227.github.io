5b050d60c774b6366e2360a8972390e2c87f5ac5
Nicole Rickard <rickard.nicole@hotmail.com>
2025-12-09 23:58:15 -0500

    updated thread tags

--- source/js/settings.js
+++ source/js/settings.js
@@ -3,5 +3,5 @@ const sheetID = `1HGxrc3juJYQpNCYSfjxSSUjVUVC7ddT6h0rA3REP0qY`;
 const oldSheetID = `xxx`;
 let successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
-const threadTags = ["vital", "rapidfire", "meetcute", "romantic", "family", "friends", "enemies", "coworkers", "notlux"];
+const threadTags = ["vital","rapidfire","fluff","angst","meetcute","notlux"];
 const chartColors = ['#74b381', '#408f8f', '#816e8f', '#a39450', '#a15a6f', '#c9986e', '#b6aa79', '#ba7878'];
